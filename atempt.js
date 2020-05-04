d00531d38968f6495f780aeb990baa005446cbaa
TimPulin <47381676+TimPulin@users.noreply.github.com>
2020-05-04 23:18:07 +0300

    Update atempt.js

--- atempt.js
+++ atempt.js
@@ -1,6 +1,29 @@
+
+//открытие и закрытие меню навигации на малых экранах
 $(document).ready(function(){
 	$('.button_open_menu').click(function(){
-		$('#main_menu').toggleClass('hide_main_menu');
+		$('ul.main_nav').toggleClass('hide_forSmScreen');
 
 	});
+});
+
+//работа с открытием в навигации списка материалов на малых экранах
+$(document).ready(function(){
+var Iam
+var Index
+	$('.dropdown_plus').click(function(){
+		Iam=$(this);
+		Index=$('.dropdown_plus').index(Iam);
+		AddRemove_Active();
+		ShowHide_dropdown_content();
+	});
+
+	function AddRemove_Active(){
+		Iam.find('a').first().toggleClass('active')
+		$('.dropdown_category').eq(Index).find('a').first().toggleClass('active');
+	};
+
+	function ShowHide_dropdown_content(){
+		$('.dropdown_content').eq(Index).toggle();
+	};
 });
