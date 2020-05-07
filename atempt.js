4d16f223ad7e132eb6c8583967b67b254b941501
TimPulin <47381676+TimPulin@users.noreply.github.com>
2020-05-07 14:27:04 +0300

    Update atempt.js

--- atempt.js
+++ atempt.js
@@ -2,7 +2,7 @@
 //открытие и закрытие меню навигации на малых экранах
 $(document).ready(function(){
 	$('.button_open_menu').click(function(){
-		$('ul.main_nav').toggleClass('hide_forSmScreen');
+		$('.main_nav').toggleClass('hide_forSmScreen');
 
 	});
 });
@@ -11,21 +11,30 @@ $(document).ready(function(){
 $(document).ready(function(){
 var Iam
 var Index
-	$('.dropdown_plus').click(function(){
+	$('.dropdown_arrow').click(function(){
 		Iam=$(this);
-		Index=$('.dropdown_plus').index(Iam);
+		Index=$('.dropdown_arrow').index(Iam);
 		Toggle_Active();
 		ShowHide_dropdown_content();//&#9650; стрелка вверх
+		ChangeArrow();
+
 	});
 
 	function Toggle_Active(){
-		Iam.find('a').first().toggleClass('active')
+		Iam.toggleClass('active')
 		$('.dropdown_category').eq(Index).toggleClass('active');
- 		$('.dropdown_category').eq(Index).find('a').first().toggleClass('active');
 	};
 
 	function ShowHide_dropdown_content(){
 		$('.dropdown_content').eq(Index).toggleClass('show_forSmScreen');
 
+	};
+
+	function ChangeArrow(){
+		if (Iam.hasClass('active')){
+			Iam.html('&#9650');
+		} else {
+			Iam.html('&#9660');
+		}
 	};
 });
